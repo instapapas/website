b1455444f9a37b79a23f2dd811d371bfa7fb365c
Matías con tilde <saitam.spatz@gmail.com>
2018-03-23 00:34:23 +0100

    Add missing library

--- install-libs.sh
+++ install-libs.sh
@@ -13,6 +13,7 @@ rm package-lock.json
 wget https://use.fontawesome.com/releases/v$FA_VERSION/fontawesome-free-$FA_VERSION.zip
 unzip fontawesome-free-$FA_VERSION.zip
 rm fontawesome-free-$FA_VERSION.zip
+mv fontawesome-free-$FA_VERSION/svg-with-js/js/fontawesome-all.min.js libs/fontawesome-all.js
 mv fontawesome-free-$FA_VERSION/svg-with-js/js/fontawesome.min.js libs/fontawesome.js
 mv fontawesome-free-$FA_VERSION/svg-with-js/js/fa-brands.min.js libs/fa-brands.js
 mv fontawesome-free-$FA_VERSION/svg-with-js/js/fa-regular.min.js libs/fa-regular.js
